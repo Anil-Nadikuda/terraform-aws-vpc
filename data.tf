813e7cd6129c7c840c4207ddcc3f356e5dee9fda
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2024-03-10 22:24:22 +0530

    first commit

--- data.tf
+++ data.tf
@@ -2,3 +2,10 @@ data "aws_availability_zones" "azs" {
     # all_availability_zones = true
     state = "available"
 }
+    data "aws_vpc" "default" {
+        default = true
+    }
+
+    data "aws_route_table" "default" {
+        
+    }
--- output.tf
+++ output.tf
@@ -2,3 +2,6 @@ output "azs" {
     value = slice(data.aws_availability_zones.azs.names,0,2)
 
 }
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
