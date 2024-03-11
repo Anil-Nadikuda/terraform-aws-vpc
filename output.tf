69f3200d9d3743cebb7faa4a94bde8e6c4bcc80e
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2024-03-11 11:55:58 +0530

    change

--- output.tf
+++ output.tf
@@ -1,13 +1,15 @@
 output "azs" {
-    value = slice(data.aws_availability_zones.azs.names,0,2)
+  value = data.aws_availability_zones.azs.names
+}
 
-}
 output "vpc_id" {
-    value = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
+
 output "public_subnet_ids" {
-    value = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
+
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
